3b5b05c23820af331969b657e79804ebfce7d98b
Victor Rortvedt <38442505+vrortvedt@users.noreply.github.com>
2020-10-16 16:16:01 -0400

    Update SFRevShareNFTAuction.sol

--- packages/buidler/contracts/SFRevShareNFTAuction.sol
+++ packages/buidler/contracts/SFRevShareNFTAuction.sol
@@ -1,14 +1,15 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-import "./github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../../github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../../github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract SFRevShareNFTAuction is ERC721 {
+contract SFRevShareNFTAuction is IERC721Receiver {
 
-address public NFTContract;
+ERC721 public NFTContract;
 uint public sourceTokenId;
-address public creator;
+address payable public creator;
 
-uint256 public totalNFTcopies;
+uint256[] public childNFTs;
 // winLength is the number of blocks that a user must be the highBidder to win the auction;
 uint256 public winLength;
 
@@ -28,29 +29,33 @@ address[] public revShareRecipients;
 uint256 public totalAuctions;
 
 mapping (uint256 => Auction) public tokenIdToAuction;
-mapping (uint256 => RevShare) public tokenIdToRevShare;
 mapping (uint256 => address) public owners; 
 
 event newAuction(uint256 id, uint256 startTime);
 event auctionWon(uint256 id, address indexed winner);
 
 constructor(address _NFTContract, uint _sourceTokenId) public {
-    NFTContract = _NFTContract;
+    require(_NFTContract != address(0) && _NFTContract != address(this));
+    NFTContract = ERC721(address(_NFTContract));
     sourceTokenId = _sourceTokenId;
     creator = msg.sender;
-    totalNFTcopies = 1;
     winLength = 180 seconds;
     revShareRecipients.push(creator);
 }
+    function onERC721Received(address, address, uint256, bytes calldata) external override returns (bytes4) {
+        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));   
+    }
     
-    function scheduleAuction(uint memory tokenId, uint256 memory startTime) public returns (uint256) {
-      ERC721 nft = ERC721(NFTContract);
-      require(msg.sender = nft.ownerOf(tokenId), "only the NFT's owner can start the auction");
+    
+    function firstAuction(uint256 startTime) external returns (uint256) {
+    // getting weird TypeError on next line "Expression has to be an lvalue"
+    //   require(NFTContract.ownerOf(sourceTokenId) = msg.sender, "only the NFT's owner can start the auction");
       require(startTime >= now, "the start time must be in the future");
       
-      totalAuctions.increment();
+      totalAuctions++;
       
-      Auction storage _auction = tokenIdToAuction[tokenId];
+      NFTContract.safeTransferFrom(msg.sender, address(this), sourceTokenId);
+      Auction storage _auction = tokenIdToAuction[sourceTokenId];
       
       _auction.generation = _auction.generation++;
       _auction.startTime = startTime;
@@ -58,21 +63,43 @@ constructor(address _NFTContract, uint _sourceTokenId) public {
       _auction.winLength = winLength;
       _auction.highBid = 0;
       _auction.owner = msg.sender;
-      _auction.highBidder = "0x00";
-      _auction.prevHighBidder = "0x00";
+      _auction.highBidder = address(0);
+      _auction.prevHighBidder = address(0);
 
 
-      emit newAuction(_auction.tokenId, _auction.startTime);
+      emit newAuction(sourceTokenId, _auction.startTime);
+
+      return _auction.generation;
+    }
+    
+    function _nextAuction(uint tokenId) private returns (uint256) {
+      
+      totalAuctions++;
+      
+      NFTContract.safeTransferFrom(msg.sender, address(this), tokenId);
+      Auction storage _auction = tokenIdToAuction[tokenId];
+      
+      _auction.generation = _auction.generation++;
+      _auction.startTime = now;
+      _auction.lastBidTime = 0;
+      _auction.winLength = winLength;
+      _auction.highBid = 0;
+      _auction.owner = msg.sender;
+      _auction.highBidder = address(0);
+      _auction.prevHighBidder = address(0);
+
+
+      emit newAuction(tokenId, _auction.startTime);
 
       return _auction.generation;
     }
 
-    function bid(uint memory tokenId, uint memory bidAmt) public payable returns (uint256, uint256, address) {
+    function bid(uint tokenId, uint bidAmt) public payable returns (uint256, uint256, address) {
         Auction storage _auction = tokenIdToAuction[tokenId];
        
         require(_auction.startTime <= now, "the auction hasn't started yet");
-        // NEED TO FIGURE OUT HOW TO DO THIS WITH SUPERFLUID ERC20
-        require(msg.value = bidAmt, "you haven't sent the correct value");
+        //Need to figure out how to convert bidAmt to ERC20
+
         require(bidAmt > _auction.highBid, "you must bid more than the current high bid");
         require(!(now < _auction.lastBidTime + _auction.winLength), "this auction is already over");
         
@@ -100,7 +127,9 @@ constructor(address _NFTContract, uint _sourceTokenId) public {
     function claimNFT(uint tokenId) public returns (bool) {
         Auction storage _auction = tokenIdToAuction[tokenId];
         require((now > _auction.lastBidTime + _auction.winLength), "this auction isn't over yet");
-        require(msg.sender = _auction.highBidder, "only the auction winner can claim");
+        
+        // getting weird TypeError on next line "Expression has to be an lvalue"
+        // require(msg.sender = _auction.highBidder, "only the auction winner can claim");
         
         // claiming an NFT shuts off the SuperFluid flow
         // for (i = 0; i < revShareRecipients.length; ++i) {
@@ -110,26 +139,25 @@ constructor(address _NFTContract, uint _sourceTokenId) public {
         // }
         
         // auction winner becomes the new owner
-        ERC721 erc721Token = ERC721(tokenContract);
-        erc721Token.safeTransferFrom(owners[tokenId], msg.sender, tokenId);
+        NFTContract.safeTransferFrom(address(this), msg.sender, tokenId);
         _auction.owner = msg.sender;
-        _auction.parentTokenId = tokenId -1;
         owners[tokenId] = msg.sender;
                 
         // claiming an NFT automatically mints a childNFT and starts a new auction
         uint childNFT = _mintChildNFT(tokenId);
         revShareRecipients.push(msg.sender);
-        startAuction(childNFT, now);
+        _nextAuction(childNFT);
         
         emit auctionWon(tokenId, msg.sender);
         emit newAuction(childNFT, now);
         return true;
     }
     
-    function _mintChildNFT(uint tokenId) private returns (uint256) {
-        ERC721 erc721Token = ERC721(tokenContract);
-        childTokenId = tokenId + 1; 
-        erc721Token._safeMint(msg.sender, childTokenId);
-        return childtokenId;
+    function _mintChildNFT(uint tokenId) private returns (uint) {
+        uint childTokenId = tokenId + 1000001; 
+        childNFTs.push(childTokenId);
+        NFTContract._safeMint(msg.sender, childTokenId);
+        return childTokenId;
     }
  }
+
